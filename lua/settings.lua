968890f87328120e44a29089dff836901fc92aa4
mvllow <mvllow@icloud.com>
2021-04-20 11:31:03 -0500

    add basic settings / keymaps

---
+++ lua/settings.lua
@@ -0,0 +1,12 @@
+vim.o.mouse = 'a'
+vim.o.hidden = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.laststatus = 0
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+
+vim.wo.number = true
+vim.wo.signcolumn = 'yes'
