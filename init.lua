86e79793597f642958658351f4ab52580939154c
mvllow <mvllow@icloud.com>
2021-04-22 20:06:42 -0500

    formatting

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
-require('plugins')
-require('settings')
-require('keymappings')
-require('colorscheme')
+require 'plugins'
+require 'settings'
+require 'keymappings'
+require 'colorscheme'
 
 -- lsp
-require('lsp')
+require 'lsp'
 
 -- plugin config
-require('p-barbar')
-require('p-compe')
-require('p-telescope')
-require('p-tree')
-require('p-treesitter')
+require 'p-barbar'
+require 'p-compe'
+require 'p-telescope'
+require 'p-tree'
+require 'p-treesitter'
