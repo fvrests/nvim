98ba9d92fd91213b1eed47c63c40116d5251fb5c
mvllow <mvllow@icloud.com>
2021-04-26 17:37:23 -0500

    update telescope find command

--- lua/p-telescope/init.lua
+++ lua/p-telescope/init.lua
@@ -1,12 +1,14 @@
 local set_keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 
-require'telescope'.setup {
-	defaults = {
-		-- use ripgrep
-		-- show hidden, but not `.git/`
-		find_command = {'rg', '--files', '--hidden', '-g', '!.git/'}
-	}
-}
+-- TODO: Why doesn't this work :(
+-- require'telescope'.setup {
+-- 	defaults = {
+-- 		-- use ripgrep
+-- 		-- show hidden, but not `.git/`
+-- 		find_command = {'rg', '--files', '--hidden', '-g', '!node_modules', '-g', '!git'}
+-- 	}
+-- }
 
-set_keymap('n', '<leader>f', ':Telescope find_files<cr>', opts)
+-- TODO: Why doesn't -g,!{.git,node_modules} work?
+set_keymap('n', '<leader>f', ':Telescope find_files find_command=rg,--hidden,--files,-g,!.git,-g,!node_modules<cr>', opts)
