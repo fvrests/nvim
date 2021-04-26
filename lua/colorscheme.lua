5862ad4eefc8dc262a11863037c2eec58408be88
mvllow <mvllow@icloud.com>
2021-04-26 18:21:53 -0500

    theme overrides

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,14 +1,24 @@
 vim.o.termguicolors = true
+
+
 vim.cmd('colorscheme nord')
 
-function fg(scope, color)
-	vim.cmd(string.format('hi %s guifg=%s', scope, color))
-end
+vim.cmd 'autocmd ColorScheme * highlight Normal guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight NonText guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight SignColumn guibg=None'
 
-local color_mod = '#88c0d0'
+vim.cmd 'autocmd ColorScheme * highlight BufferTabpageFill guibg=None'
 
--- TODO: This seems to be overwritten by colorscheme
-fg('BufferCurrentMod', color_mod)
-fg('BufferInactiveMod', color_mod)
-fg('BufferVisibleMod', color_mod)
+vim.cmd 'autocmd ColorScheme * highlight BufferCurrent guifg=#e0def4 guibg=#26233a'
+vim.cmd 'autocmd ColorScheme * highlight BufferCurrentMod guifg=#88c0d0 guibg=#26233a'
+vim.cmd 'autocmd ColorScheme * highlight BufferCurrentSign guifg=#e0def4 guibg=#26233a'
+
+vim.cmd 'autocmd ColorScheme * highlight BufferInactive guifg=#6e6a86 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferInactiveMod guifg=#88c0d0 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferInactiveSign guifg=#6e6a86 guibg=None'
+
+vim.cmd 'autocmd ColorScheme * highlight BufferVisible guifg=#e0def4 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferVisibleMod guifg=#88c0d0 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferVisibleSign guifg=#6e6a86 guibg=None'
+
 
