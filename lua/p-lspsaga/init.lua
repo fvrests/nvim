daf94da8f8cead296e11dd3cbc2cad75a3a55dc4
mvllow <mvllow@icloud.com>
2021-04-27 12:16:11 -0500

    use lua commands

--- lua/p-lspsaga/init.lua
+++ lua/p-lspsaga/init.lua
@@ -3,19 +3,18 @@ require'lspsaga'.init_lsp_saga()
 local set_keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 
--- lsp provider to find the cursor word definition and reference
-set_keymap('n', 'gh', ':Lspsaga lsp_finder<cr>', opts)
--- code action
-set_keymap('n', '<leader>ca', ':Lspsaga code_action<cr>', opts)
-set_keymap('v', '<leader>ca', ':Lspsaga range_code_action<cr>', opts)
--- show hover doc
-set_keymap('n', 'K', ':Lspsaga hover_doc<cr>', opts)
--- show diagnostics
-set_keymap('n', 'H', ':Lspsaga show_line_diagnostics<cr>', opts)
--- show signature help
-set_keymap('n', 'gs', ':Lspsaga signature_help<cr>', opts)
+-- hover doc
+set_keymap('n', 'K', [[<cmd>lua require'lspsaga.hover'.render_hover_doc()<cr>]], opts)
+-- diagnostics
+set_keymap('n', 'H', [[<cmd>lua require'lspsaga.diagnostics'.show_cursor_diagnostics()<cr>]], opts)
+-- definition / reference
+set_keymap('n', 'gh', [[<cmd>lua require'lspsaga.provider'.lsp_finder()<cr>]], opts)
+-- signature help
+set_keymap('n', 'gs', [[<cmd>lua require'lspsaga.signaturehelp'.signature_help()<cr>]], opts)
 -- rename
-set_keymap('n', 'gr', ':Lspsaga rename<cr>', opts)
+set_keymap('n', 'gr', [[<cmd>lua require'lspsaga.rename'.rename()<cr>]], opts)
 -- preview definition
-set_keymap('n', 'gd', ':Lspsaga preview_definition<cr>', opts)
-
+set_keymap('n', 'gd', [[<cmd>lua require'lspsaga.provider'.preview_definition()<cr>]], opts)
+-- code action
+set_keymap('n', '<leader>ca', [[<cmd>lua require'lspsaga.codeaction'.code_action()<cr>]], opts)
+set_keymap('v', '<leader>ca', [[<cmd>lua require'lspsaga.codeaction'.range_code_action()<cr>]], opts)
