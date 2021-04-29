c95c6dd094cd7f125bf5cea7a45b80a3ea46cb23
mvllow <mvllow@icloud.com>
2021-04-29 17:29:11 -0500

    note bug with formatter

--- lua/autocommands/init.lua
+++ lua/autocommands/init.lua
@@ -46,5 +46,7 @@ define_augroups({
         {'BufRead', '*', 'setlocal formatoptions-=c formatoptions-=r formatoptions-=o'},
         {'BufNewFile', '*', 'setlocal formatoptions-=c formatoptions-=r formatoptions-=o'},
     },
+	-- TODO: We should check if the formatter, eg. prettier, exists
+	-- If not, there is a large delay before saving
 	_auto_formatters = auto_formatters
 })
