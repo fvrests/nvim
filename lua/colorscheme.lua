acc2958e9cddbfb3db8717dae31886ad08199768
mvllow <mvllow@icloud.com>
2021-04-29 12:52:50 -0500

    override barbar highlights

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,6 +1,3 @@
-local set_keymap = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
-
 vim.o.termguicolors = true
 
 require('colorbuddy').colorscheme('nordbuddy')
@@ -10,20 +7,17 @@ require('colorbuddy').colorscheme('nordbuddy')
 -- vim.cmd 'autocmd ColorScheme * highlight SignColumn guibg=None'
 -- vim.cmd 'autocmd ColorScheme * highlight StatusLine guibg=None'
 -- vim.cmd 'autocmd ColorScheme * highlight StatusLineNC guibg=None'
--- vim.cmd 'autocmd ColorScheme * highlight VertSplit guifg=#26233a guibg=None'
--- 
--- vim.cmd 'autocmd ColorScheme * highlight BufferTabpageFill guibg=None'
--- 
--- vim.cmd 'autocmd ColorScheme * highlight BufferCurrent guifg=#e0def4 guibg=#26233a'
--- vim.cmd 'autocmd ColorScheme * highlight BufferCurrentMod guifg=#88c0d0 guibg=#26233a'
--- vim.cmd 'autocmd ColorScheme * highlight BufferCurrentSign guifg=#e0def4 guibg=#26233a'
--- 
--- vim.cmd 'autocmd ColorScheme * highlight BufferInactive guifg=#6e6a86 guibg=None'
--- vim.cmd 'autocmd ColorScheme * highlight BufferInactiveMod guifg=#88c0d0 guibg=None'
--- vim.cmd 'autocmd ColorScheme * highlight BufferInactiveSign guifg=#6e6a86 guibg=None'
--- 
--- vim.cmd 'autocmd ColorScheme * highlight BufferVisible guifg=#e0def4 guibg=None'
--- vim.cmd 'autocmd ColorScheme * highlight BufferVisibleMod guifg=#88c0d0 guibg=None'
--- vim.cmd 'autocmd ColorScheme * highlight BufferVisibleSign guifg=#6e6a86 guibg=None'
+-- vim.cmd 'autocmd ColorScheme * highlight VertSplit guifg=None guibg=None'
+
+vim.cmd 'autocmd ColorScheme * highlight BufferTabpageFill guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferCurrent guifg=#eceff4 guibg=#434c5e'
+vim.cmd 'autocmd ColorScheme * highlight BufferCurrentMod guifg=#88c0d0 guibg=#434c5e'
+vim.cmd 'autocmd ColorScheme * highlight BufferCurrentSign guifg=#eceff4 guibg=#434c5e'
+vim.cmd 'autocmd ColorScheme * highlight BufferInactive guifg=#d8dee9 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferInactiveMod guifg=#88c0d0 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferInactiveSign guifg=#d8dee9 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferVisible guifg=#d8dee9 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferVisibleMod guifg=#88c0d0 guibg=None'
+vim.cmd 'autocmd ColorScheme * highlight BufferVisibleSign guifg=#d8dee9 guibg=None'
 
 
