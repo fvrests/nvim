e7b70beb29e8ff14d28ba2534cf3632b0cc3c9d9
mvllow <mvllow@icloud.com>
2021-04-28 23:16:21 -0500

    remove unused efm config

--- lua/lsp/efm.lua
+++ lua/lsp/efm.lua
@@ -1,17 +1,9 @@
 local lspconfig = require 'lspconfig'
 
--- efm.lua
 local prettier = {
-  formatCommand = "./node_modules/.bin/prettier --stdin-filepath ${INPUT}",
+  formatCommand = './node_modules/.bin/prettier --stdin-filepath ${INPUT}',
   formatStdin = true
 }
-
--- local eslint = {
---   lintCommand = 'eslint -f unix --stdin',
---   lintIgnoreExitCode = true,
---   lintStdin = true,
--- }
-
 
 local languages = {
 	json = { prettier },
@@ -27,14 +19,14 @@ local languages = {
 }
 
 return {
-	root_dir = lspconfig.util.root_pattern("yarn.lock", "package.json", ".git"),
+	root_dir = lspconfig.util.root_pattern('.git', 'package.json'),
 	filetypes = vim.tbl_keys(languages),
 	init_options = {
 		documentFormatting = true,
 		codeAction = true
 	},
 	settings = {
-		rootMarkers = { ".git/" },
+		rootMarkers = { '.git/' },
 		languages = languages
 	}
 }
