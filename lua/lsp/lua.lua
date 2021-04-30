6915968ed64f6553e6f76dd0e911d9a372fdc553
mvllow <mvllow@icloud.com>
2021-04-29 22:52:34 -0500

    wip: add lua lsp cmd

--- lua/lsp/lua.lua
+++ lua/lsp/lua.lua
@@ -1,4 +1,8 @@
+local sumneko_root_path = vim.fn.stdpath('data') .. "/lspinstall/lua"
+local sumneko_binary = sumneko_root_path .. "/sumneko-lua-language-server"
+
 return {
+	cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
 	settings = {
 		Lua = {
 			runtime = {
