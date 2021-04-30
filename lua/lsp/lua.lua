1de31a16e13a161761f17c36c70651b7816e31ce
mvllow <mvllow@icloud.com>
2021-04-29 22:25:55 -0500

    fix lua table

--- lua/lsp/lua.lua
+++ lua/lsp/lua.lua
@@ -1,20 +1,22 @@
 return {
-	Lua = {
-		runtime = {
-			version = 'LuaJIT',
-			path = vim.split(package.path, ';'),
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+				path = vim.split(package.path, ';'),
+			},
+			diagnostics = {
+				globals = {'vim'},
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+					[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+				},
+			},
+			telemetry = {
+				enable = false,
+			},
 		},
-		diagnostics = {
-			globals = {'vim'},
-		},
-		workspace = {
-			library = {
-				[vim.fn.expand('$VIMRUNTIME/lua')] = true,
-				[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-			}
-		},
-		telemetry = {
-			enable = false,
-		},
-	}
+	},
 }
