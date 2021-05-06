b23c2d7eaad10f6a51c6bb79c0d3a7f2296deb19
mvllow <mvllow@icloud.com>
2021-05-06 13:31:54 -0500

    stylua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,34 +2,31 @@ local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.n
 
 -- Install packer (plugin manager) if not already
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
 	vim.api.nvim_command('packadd packer.nvim')
 end
 
 -- Auto compile changed plugins
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+vim.cmd('autocmd BufWritePost plugins.lua PackerCompile')
 
-return require('packer').startup(
-	function(use)
-		use 'wbthomason/packer.nvim'
-		use 'romgrk/barbar.nvim'
-		use {'iamcco/markdown-preview.nvim', ft = {'markdown'}, run = 'cd app && npm install'}
-		use {
-			'nvim-telescope/telescope.nvim',
-			requires = {'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim'}
-		}
-		-- use 'kyazdani42/nvim-web-devicons'
-		use 'kyazdani42/nvim-tree.lua'
-		use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-		use 'JoosepAlviste/nvim-ts-context-commentstring'
-		use 'windwp/nvim-ts-autotag'
-		use 'rktjmp/lush.nvim'
-		use 'neovim/nvim-lspconfig'
-		use 'kabouzeid/nvim-lspinstall'
-		use 'hrsh7th/nvim-compe'
-		use 'hrsh7th/vim-vsnip'
-		use 'rafamadriz/friendly-snippets'
-		use 'terrortylor/nvim-comment'
-	end
-)
-
+return require('packer').startup(function(use)
+	use('wbthomason/packer.nvim')
+	use('romgrk/barbar.nvim')
+	use({ 'iamcco/markdown-preview.nvim', ft = { 'markdown' }, run = 'cd app && npm install' })
+	use({
+		'nvim-telescope/telescope.nvim',
+		requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
+	})
+	-- use 'kyazdani42/nvim-web-devicons'
+	use('kyazdani42/nvim-tree.lua')
+	use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+	use('JoosepAlviste/nvim-ts-context-commentstring')
+	use('windwp/nvim-ts-autotag')
+	use('rktjmp/lush.nvim')
+	use('neovim/nvim-lspconfig')
+	use('kabouzeid/nvim-lspinstall')
+	use('hrsh7th/nvim-compe')
+	use('hrsh7th/vim-vsnip')
+	use('rafamadriz/friendly-snippets')
+	use('terrortylor/nvim-comment')
+end)
