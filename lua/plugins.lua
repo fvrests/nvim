fa09eaa213eaeffda0785e3f9584e04ecdec6f06
mvllow <mvllow@icloud.com>
2021-05-10 10:32:58 -0500

    organize plugin list

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,24 +10,40 @@ end
 vim.cmd('autocmd BufWritePost plugins.lua PackerCompile')
 
 return require('packer').startup(function(use)
+	-- plugin manager
 	use('wbthomason/packer.nvim')
+
+	-- bufferline
 	use('romgrk/barbar.nvim')
-	use({ 'iamcco/markdown-preview.nvim', ft = { 'markdown' }, run = 'cd app && npm install' })
+
+	-- file explorer
+	use('kyazdani42/nvim-tree.lua')
+
+	-- fuzzy finder
 	use({
 		'nvim-telescope/telescope.nvim',
 		requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
 	})
-	-- use 'kyazdani42/nvim-web-devicons'
-	use('kyazdani42/nvim-tree.lua')
+
+	-- treesitter
 	use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
-	use('JoosepAlviste/nvim-ts-context-commentstring')
 	use('windwp/nvim-ts-autotag')
+	use('JoosepAlviste/nvim-ts-context-commentstring')
+
+	-- comments
+	use('terrortylor/nvim-comment')
+
+	-- theming
 	use('rktjmp/lush.nvim')
+
+	-- lsp / completions
 	use('neovim/nvim-lspconfig')
 	use('kabouzeid/nvim-lspinstall')
 	use('hrsh7th/nvim-compe')
 	use('hrsh7th/vim-vsnip')
 	use('rafamadriz/friendly-snippets')
-	use('terrortylor/nvim-comment')
 	use('jose-elias-alvarez/nvim-lsp-ts-utils')
+
+	-- markdown preview
+	use({ 'iamcco/markdown-preview.nvim', ft = { 'markdown' }, run = 'cd app && npm install' })
 end)
