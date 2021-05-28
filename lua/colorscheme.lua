ba4f1d8349a1cfcd7e1f718dffc47de7229c7e43
mvllow <mvllow@icloud.com>
2021-05-28 15:02:05 -0500

    change toggle theme keymap to <leader>t

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -4,4 +4,6 @@ vim.cmd('colorscheme ' .. O.colorscheme)
 local set_keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-set_keymap('n', '<c-m>', [[<cmd>lua require('rose-pine.functions').toggle_variant({'base', 'dawn'})<cr>]], opts)
+if O.colorscheme == 'rose-pine' then
+	set_keymap('n', '<leader>t', [[<cmd>lua require('rose-pine.functions').toggle_variant({'base', 'dawn'})<cr>]], opts)
+end
