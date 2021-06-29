6dae9c2c821f5891058bd442ffc5425d19bcff73
mvllow <mvllow@icloud.com>
2021-06-29 11:14:44 -0500

    fix: format pre write

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,7 +2,7 @@ local on_attach = function(client)
 	if client.resolved_capabilities.document_formatting then
 		vim.api.nvim_command([[augroup Format]])
 		vim.api.nvim_command([[autocmd! * <buffer>]])
-		vim.api.nvim_command([[autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting_seq_sync(nil, 1000)]])
+		vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync(nil, 1000)]])
 		vim.api.nvim_command([[augroup END]])
 	end
 	vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' })
