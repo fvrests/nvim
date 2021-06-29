5638f13b0eb9b8a167326b2a5f89369e7452b3c5
mvllow <mvllow@icloud.com>
2021-06-29 11:13:37 -0500

    feat: add nvim which key

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,6 +19,12 @@ packer.init({
 packer.startup(function(use)
 	use("wbthomason/packer.nvim")
 
+	use {
+	  "folke/which-key.nvim",
+	  config = function()
+		require("which-key").setup {}
+	  end
+	}
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		config = function()
