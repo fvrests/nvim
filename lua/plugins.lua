220be2c6d153984b1b40a8e63d075de366484957
mvllow <mvllow@icloud.com>
2021-07-01 09:27:04 -0500

    feat: add gitsigns (incl statusline)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -147,4 +147,11 @@ packer.startup(function(use)
 		requires = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' },
 	})
 
+	use({
+		'lewis6991/gitsigns.nvim',
+		config = function()
+			require('gitsigns').setup()
+		end,
+		requires = { 'nvim-lua/plenary.nvim' },
+	})
 end)
