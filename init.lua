2bca0af50465f4a9f0e27d7652563f4cdd2aa08c
mvllow <mvllow@icloud.com>
2021-07-04 21:44:45 -0500

    update init

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
-require('plugins')
 require('globals')
-vim.cmd('luafile ~/.config/nvim/conf.lua')
+vim.cmd('luafile ' .. vim.fn.stdpath('config') .. '/conf.lua')
 require('settings')
+require('plugins')
+vim.g.colors_name = O.colorscheme
+vim.g.syntax = true
 require('keymappings')
 require('lsp')
