a23f1a5e40bca8be1efe1e468461c03936716e58
mvllow <mvllow@icloud.com>
2021-07-06 11:44:36 -0500

    check for nil colorscheme

--- init.lua
+++ init.lua
@@ -2,7 +2,10 @@ require('globals')
 vim.cmd('luafile ' .. vim.fn.stdpath('config') .. '/conf.lua')
 require('settings')
 require('plugins')
-vim.g.colors_name = O.colorscheme
-vim.g.syntax = true
+if O.editor.colorscheme ~= nil then
+	vim.cmd('colorscheme ' .. O.editor.colorscheme)
+else
+	vim.g.syntax = false
+end
 require('keymappings')
 require('lsp')
