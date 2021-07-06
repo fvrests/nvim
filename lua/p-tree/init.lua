9b0364d173b2f91749651f8cb6910b7851fd9d2e
mvllow <mvllow@icloud.com>
2021-07-06 15:05:28 -0500

    open node in default app

--- lua/p-tree/init.lua
+++ lua/p-tree/init.lua
@@ -1,3 +1,11 @@
+function NvimTreeOSOpen()
+	local lib = require('nvim-tree.lib')
+	local node = lib.get_node_at_cursor()
+	if node then
+		vim.fn.jobstart("open '" .. node.absolute_path .. "' &", { detach = true })
+	end
+end
+
 vim.g.nvim_tree_show_icons = {
 	git = 1,
 	folders = 1,
@@ -41,6 +49,7 @@ local tree_cb = require('nvim-tree.config').nvim_tree_callback
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_bindings = {
 	{ key = 'v', cb = tree_cb('vsplit') },
+	{ key = 'o', cb = ':lua NvimTreeOSOpen()<cr>' },
 }
 vim.g.nvim_tree_ignore = { '.git' }
 vim.g.nvim_tree_lsp_diagnostics = true
