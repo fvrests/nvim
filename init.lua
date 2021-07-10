232c8f4a18413baf58b37b9a9049553343da98d7
mvllow <mvllow@icloud.com>
2021-07-09 21:10:38 -0500

    disable loading built ins

--- init.lua
+++ init.lua
@@ -1,3 +1,28 @@
+local disabled_built_ins = {
+	'netrw',
+	'netrwPlugin',
+	'netrwSettings',
+	'netrwFileHandlers',
+	'gzip',
+	'zip',
+	'zipPlugin',
+	'tar',
+	'tarPlugin',
+	'man',
+	'getscript',
+	'getscriptPlugin',
+	'vimball',
+	'vimballPlugin',
+	'2html_plugin',
+	'logipat',
+	'rrhelper',
+	'spellfile_plugin',
+	-- 'matchit', 'matchparen', 'shada_plugin',
+}
+for _, plugin in pairs(disabled_built_ins) do
+	vim.g['loaded_' .. plugin] = 1
+end
+
 require('globals')
 vim.cmd('luafile ' .. vim.fn.stdpath('config') .. '/conf.lua')
 require('settings')
