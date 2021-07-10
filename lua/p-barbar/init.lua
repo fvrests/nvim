519bd2658238cafd1d73767dbb1656b95b29fe85
mvllow <mvllow@icloud.com>
2021-07-09 22:20:22 -0500

    update to unicode icons

--- lua/p-barbar/init.lua
+++ lua/p-barbar/init.lua
@@ -1,7 +1,9 @@
 vim.g.bufferline = {
 	animation = false,
+	icon_close_tab = '✖',
+	icon_close_tab_modified = '●',
 	icon_separator_active = '',
 	icon_separator_inactive = '',
 	icons = false,
-	no_name_title = '[ New buffer ]',
+	no_name_title = '(?)',
 }
