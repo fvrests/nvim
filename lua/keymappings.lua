650adae39b904bef2c078a93b72a8821f3c351ac
mvllow <mvllow@icloud.com>
2021-07-12 16:51:36 -0500

    use which key for leader keymaps

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,23 +1,53 @@
-vim.api.nvim_set_keymap('n', '<space>', '<nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	'n',
+	'<space>',
+	'<nop>',
+	{ noremap = true, silent = true }
+)
 vim.g.mapleader = ' '
 
 -- clear highlights
-vim.api.nvim_set_keymap('n', '<esc>', ':noh<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	'n',
+	'<esc>',
+	':noh<cr>',
+	{ noremap = true, silent = true }
+)
 
 -- simplify split movements
-vim.api.nvim_set_keymap('n', '<c-h>', '<c-w><c-h>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<c-j>', '<c-w><c-j>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<c-k>', '<c-w><c-k>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<c-l>', '<c-w><c-l>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>wf', '<c-w><c-r>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	'n',
+	'<c-h>',
+	'<c-w><c-h>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	'n',
+	'<c-j>',
+	'<c-w><c-j>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	'n',
+	'<c-k>',
+	'<c-w><c-k>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	'n',
+	'<c-l>',
+	'<c-w><c-l>',
+	{ noremap = true, silent = true }
+)
+-- vim.api.nvim_set_keymap('n', '<leader>wf', '<c-w><c-r>', { noremap = true, silent = true })
 
 -- move through wrapped lines
 vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
 
 -- toggle word highlight
-vim.api.nvim_set_keymap('n', '<leader>h', ':let @/=""<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>h', ':set hlsearch!<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>h', ':let @/=""<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>h', ':set hlsearch!<cr>', { noremap = true, silent = true })
 
 -- reselect after visual indent
 vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
@@ -25,62 +55,87 @@ vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
 
 -- search visually selected text (consistent `*` behaviour)
 vim.api.nvim_set_keymap('n', '*', [[*N]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '*', [[y/\V<c-r>=escape(@",'/\')<cr><cr>N]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	'v',
+	'*',
+	[[y/\V<c-r>=escape(@",'/\')<cr><cr>N]],
+	{ noremap = true, silent = true }
+)
 
 -- bubble lines
-vim.api.nvim_set_keymap('x', 'J', ":move '>+1<cr>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x', 'K', ":move '<-2<cr>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	'x',
+	'J',
+	":move '>+1<cr>gv=gv",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	'x',
+	'K',
+	":move '<-2<cr>gv=gv",
+	{ noremap = true, silent = true }
+)
 
 -- escape
 vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true, silent = true })
 
 -- packer
-vim.api.nvim_set_keymap('n', '<leader>pi', ':PackerInstall<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>pc', ':PackerCompile<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ps', ':PackerSync<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>pi', ':PackerInstall<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>pc', ':PackerCompile<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>ps', ':PackerSync<cr>', { noremap = true, silent = true })
 
 -- nvim-tree
-vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<cr>', { noremap = true, silent = true })
 
 -- barbar
-vim.api.nvim_set_keymap('n', 'L', ':BufferNext<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'H', ':BufferPrevious<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>bd', ':BufferClose<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>bo', ':BufferCloseAllButCurrent<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	'n',
+	'L',
+	':BufferNext<cr>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	'n',
+	'H',
+	':BufferPrevious<cr>',
+	{ noremap = true, silent = true }
+)
+-- vim.api.nvim_set_keymap('n', '<leader>bd', ':BufferClose<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>bo', ':BufferCloseAllButCurrent<cr>', { noremap = true, silent = true })
 
 -- telescope
-vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope find_files<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>/', ':Telescope live_grep<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope find_files<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>/', ':Telescope live_grep<cr>', { noremap = true, silent = true })
 
 -- treesitter
-vim.api.nvim_set_keymap('n', '<leader>th', ':TSBufToggle highlight<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tc', ':TSHighlightCapturesUnderCursor<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>th', ':TSBufToggle highlight<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>tc', ':TSHighlightCapturesUnderCursor<cr>', { noremap = true, silent = true })
 
 -- lsp
-vim.api.nvim_set_keymap('n', '<leader>ldc', ':lua vim.lsp.buf.declaration()<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ldf', ':lua vim.lsp.buf.definition()<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>li', ':LspInfo<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap(
-	'n',
-	'<leader>lk',
-	':lua vim.lsp.diagnostic.show_line_diagnostics()<cr>',
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap('n', '<leader>lf', ':lua vim.lsp.buf.formatting()<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ltd', ':lua vim.lsp.buf.type_definition()<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>lrn', ':lua vim.lsp.buf.rename()<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>lca', ':lua vim.lsp.buf.code_action()<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>lre', ':lua vim.lsp.buf.references()<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>ldc', ':lua vim.lsp.buf.declaration()<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>ldf', ':lua vim.lsp.buf.definition()<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>li', ':LspInfo<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap(
+-- 	'n',
+-- 	'<leader>lk',
+-- 	':lua vim.lsp.diagnostic.show_line_diagnostics()<cr>',
+-- 	{ noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap('n', '<leader>lf', ':lua vim.lsp.buf.formatting()<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>ltd', ':lua vim.lsp.buf.type_definition()<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>lrn', ':lua vim.lsp.buf.rename()<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>lca', ':lua vim.lsp.buf.code_action()<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>lre', ':lua vim.lsp.buf.references()<cr>', { noremap = true, silent = true })
 
 -- formatter.nvim
-vim.api.nvim_set_keymap('n', '<leader>lf', ':Format<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap(
-	'n',
-	'<leader>lF',
-	":lua require('utils').save_without_formatting()<cr>",
-	{ noremap = true, silent = true }
-)
+-- vim.api.nvim_set_keymap('n', '<leader>lf', ':Format<cr>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap(
+-- 	'n',
+-- 	'<leader>lF',
+-- 	":lua require('utils').save_without_formatting()<cr>",
+-- 	{ noremap = true, silent = true }
+-- )
 
 -- nvim-compe
 local t = function(str)
@@ -117,13 +172,38 @@ _G.s_tab_complete = function()
 	end
 end
 
-vim.api.nvim_set_keymap('i', '<tab>', 'v:lua.tab_complete()', { silent = true, expr = true })
-vim.api.nvim_set_keymap('s', '<tab>', 'v:lua.tab_complete()', { silent = true, expr = true })
-vim.api.nvim_set_keymap('i', '<s-tab>', 'v:lua.s_tab_complete()', { silent = true, expr = true })
-vim.api.nvim_set_keymap('s', '<s-tab>', 'v:lua.s_tab_complete()', { silent = true, expr = true })
+vim.api.nvim_set_keymap(
+	'i',
+	'<tab>',
+	'v:lua.tab_complete()',
+	{ silent = true, expr = true }
+)
+vim.api.nvim_set_keymap(
+	's',
+	'<tab>',
+	'v:lua.tab_complete()',
+	{ silent = true, expr = true }
+)
+vim.api.nvim_set_keymap(
+	'i',
+	'<s-tab>',
+	'v:lua.s_tab_complete()',
+	{ silent = true, expr = true }
+)
+vim.api.nvim_set_keymap(
+	's',
+	'<s-tab>',
+	'v:lua.s_tab_complete()',
+	{ silent = true, expr = true }
+)
 -- vim.api.nvim_set_keymap('i', '<cr>', [[compe#confirm('<cr>')]], { silent = true, expr = true })
 require('nvim-autopairs.completion.compe').setup({
 	map_cr = true, --  map <cr> on insert mode
 	map_complete = true, -- it will auto insert `(` after select function or method item
 })
-vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { silent = true, expr = true })
+vim.api.nvim_set_keymap(
+	'i',
+	'<c-space>',
+	'compe#complete()',
+	{ silent = true, expr = true }
+)
--- lua/p-which-key/init.lua
+++ lua/p-which-key/init.lua
@@ -1,1 +1,110 @@
-require('which-key').setup({})
+local wk = require('which-key')
+
+wk.register({
+	['c'] = { '<cmd>BufferClose<cr>', 'Close buffer' },
+	['e'] = { '<cmd>NvimTreeToggle<cr>', 'Toggle explorer' },
+	['f'] = { '<cmd>Telescope find_files<cr>', 'Find file' },
+	['h'] = { '<cmd>let @/=""<cr>', 'Clear match highlight' },
+	b = {
+		name = 'Buffer',
+		c = { '<cmd>BufferClose<cr>', 'Close' },
+		o = { '<cmd>BufferCloseAllButCurrent<cr>', 'Close others' },
+		n = { '<cmd>BufferNext<cr>', 'Next' },
+		p = { '<cmd>BufferPrevious<cr>', 'Prev' },
+		r = { '<c-w><c-r>', 'Swap splits' },
+	},
+	p = {
+		name = 'Packer',
+		c = { '<cmd>PackerCompile<cr>', 'Compile' },
+		i = { '<cmd>PackerInstall<cr>', 'Install' },
+		r = { "<cmd>lua require('utils').reload_config()<cr>", 'Reload' },
+		s = { '<cmd>PackerSync<cr>', 'Sync' },
+		u = { '<cmd>PackerUpdate<cr>', 'Update' },
+	},
+	g = {
+		name = 'Git',
+		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", 'Next hunk' },
+		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", 'Prev hunk' },
+		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", 'Blame' },
+		p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", 'Preview hunk' },
+		r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", 'Reset hunk' },
+		R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", 'Reset buffer' },
+		s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", 'Stage hunk' },
+		u = {
+			"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+			'Undo stage hunk',
+		},
+		o = { '<cmd>Telescope git_status<cr>', 'Open changed file' },
+		b = { '<cmd>Telescope git_branches<cr>', 'Checkout branch' },
+		c = { '<cmd>Telescope git_commits<cr>', 'Checkout commit' },
+		C = {
+			'<cmd>Telescope git_bcommits<cr>',
+			'Checkout commit (for current file)',
+		},
+	},
+	l = {
+		name = 'LSP',
+		a = { '<cmd>lua vim.lsp.buf.code_action()<cr>', 'Code action' },
+		d = {
+			'<cmd>Telescope lsp_document_diagnostics<cr>',
+			'Document diagnostics',
+		},
+		e = { '<cmd>lua vim.lsp.buf.references()<cr>', 'References' },
+		o = { '<cmd>lua vim.lsp.buf.declaration()<cr>', 'Declaration' },
+		O = { '<cmd>lua vim.lsp.buf.definition()<cr>', 'Definition' },
+		h = { '<cmd>lua vim.lsp.buf.hover()<cr>', 'Hover' },
+		w = {
+			'<cmd>Telescope lsp_workspace_diagnostics<cr>',
+			'Workspace diagnostics',
+		},
+		i = { '<cmd>LspInfo<cr>', 'Info' },
+		j = { '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', 'Next diagnostic' },
+		k = { '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', 'Prev diagnostic' },
+		l = {
+			'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>',
+			'Line diagnostics',
+		},
+		q = { '<cmd>Telescope quickfix<cr>', 'Quickfix' },
+		r = { '<cmd>lua vim.lsp.buf.rename()<cr>', 'Rename' },
+		s = { '<cmd>Telescope lsp_document_symbols<cr>', 'Document symbols' },
+		S = {
+			'<cmd>Telescope lsp_dynamic_workspace_symbols<cr>',
+			'Workspace symbols',
+		},
+		t = {
+			'<cmd>lua vim.lsp.buf.type_definition()<cr>',
+			'Type definition',
+		},
+
+		-- TODO: Maybe find a better spot for formatting
+		f = { '<cmd>silent FormatWrite<cr>', 'Format' },
+		F = {
+			"<cmd>lua require('utils').save_without_formatting()<cr>",
+			'Save without formatting',
+		},
+	},
+	s = {
+		name = 'Search',
+		b = { '<cmd>Telescope git_branches<cr>', 'Branches' },
+		c = { '<cmd>Telescope colorscheme<cr>', 'Colorscheme' },
+		C = { '<cmd>Telescope commands<cr>', 'Commands' },
+		f = { '<cmd>Telescope find_files<cr>', 'Files' },
+		h = { '<cmd>Telescope help_tags<cr>', 'Help tags' },
+		k = { '<cmd>Telescope keymaps<cr>', 'Keymaps' },
+		M = { '<cmd>Telescope man_pages<cr>', 'Man pages' },
+		r = { '<cmd>Telescope oldfiles<cr>', 'Recent files' },
+		R = { '<cmd>Telescope registers<cr>', 'Registers' },
+		t = { '<cmd>Telescope live_grep<cr>', 'Text' },
+	},
+	T = {
+		name = 'Treesitter',
+		i = { '<cmd>TSConfigInfo<cr>', 'Info' },
+		b = { '<cmd>TSBufToggle highlight', 'Toggle highlight' },
+		c = {
+			'<cmd>TSHighlightCapturesUnderCursor<cr>',
+			'Highlight active captures',
+		},
+	},
+}, {
+	prefix = '<leader>',
+})
