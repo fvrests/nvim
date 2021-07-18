c4d89a6d438b68f0f0aa8c552443d0dbb07fd20c
mvllow <mvllow@icloud.com>
2021-07-18 16:27:12 -0500

    cleanup

--- lua/modules/autopairs.lua
+++ lua/modules/autopairs.lua
@@ -8,12 +8,3 @@ if package.loaded['compe'] then
 		map_complete = true, -- it will auto insert `(` after select function or method item
 	})
 end
-
--- TODO: Where do we want ts plugin config?
--- 1. Keep ts plugin config with main treesitter file (current solution)
--- 2. Move ts plugin config to respective files
--- require('nvim-treesitter.configs').setup({
--- 	autopairs = {
--- 		enable = true,
--- 	},
--- })
