e7a72f0177007e4b296a779e6aa7233443a2c532
mvllow <mvllow@icloud.com>
2021-07-17 23:41:39 -0500

    cleanup

--- lua/utils.lua
+++ lua/utils.lua
@@ -3,7 +3,6 @@ local utils = {}
 function utils.reload_config()
 	vim.cmd('source ~/.config/nvim/conf.lua')
 	vim.cmd('source ~/.config/nvim/lua/plugins.lua')
-	-- vim.cmd('source ~/.config/nvim/lua/p-formatter/init.lua')
 	vim.cmd(':PackerCompile')
 	vim.cmd(':PackerInstall')
 end
