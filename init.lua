e3e67def4615b52472c8aa17e3f62e8b4afb96d4
mvllow <mvllow@icloud.com>
2021-07-17 23:37:53 -0500

    remove colorscheme checks

--- init.lua
+++ init.lua
@@ -28,19 +28,6 @@ vim.cmd('luafile ' .. vim.fn.stdpath('config') .. '/conf.lua')
 require('settings')
 require('plugins')
 require('utils')
-if O.editor.colorscheme ~= nil then
-	vim.cmd('colorscheme ' .. O.editor.colorscheme)
-else
-	-- Disable all syntax highlighting
-	-- Goal(wip): Make a usable environment with no colour
-	--   * Remove any default colours, eg. line numbers
-
-	O.noop.treesitter.highlight.enable = false
-	vim.cmd('syntax off')
-	vim.cmd('set nohlsearch')
-	vim.cmd('set t_Co=0')
-	-- Fix barbar complaining about `Special`
-	vim.cmd('hi Special ctermfg=None')
-end
+vim.cmd('colorscheme ' .. O.editor.colorscheme)
 require('keymappings')
 require('lsp')
