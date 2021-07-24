a7fad42888bb087e818ae8565abc3e775893009c
mvllow <mvllow@icloud.com>
2021-07-24 13:37:29 -0500

    fix: opt_local not working for spell setting

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,6 +1,6 @@
 local tw = 2
 vim.opt_local.expandtab = true
-vim.opt_local.spell = true
+vim.cmd('setlocal spell')
 vim.opt_local.shiftwidth = tw
 vim.opt_local.softtabstop = tw
 vim.opt_local.tabstop = tw
