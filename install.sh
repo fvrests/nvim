b4d76a88f54ec018c755162e1d40b13874b7f347
mvllow <mvllow@icloud.com>
2021-07-29 00:28:59 -0500

    refactor installer

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "mvllow/nvim\n"
 nvim_config_path="$HOME/.config/nvim"
 nvim_resource_path="$HOME/.local/share/nvim"
 
-maybe_delete_neovim_config() {
+if [ -e $nvim_config_path -o -e $nvim_resource_path ]; then
 	echo "\033[1;31mThe following will be deleted:\033[0m"
 	echo
 	echo "  $nvim_config_path"
@@ -21,35 +21,6 @@ maybe_delete_neovim_config() {
 		echo
 		exit 2
 	fi
-}
-
-while getopts oh option; do
-	case "${option}" in
-	o) maybe_delete_neovim_config ;;
-	h)
-		echo "\033[0;90m  Usage\033[0m\n"
-		echo "    $ sh $0 <options>"
-		echo
-		echo "\033[0;90m  Options\033[0m\n"
-		echo "    -o  overwrite existing neovim config"
-		echo "    -h  show this message"
-		echo
-		echo "\033[0;90m  Examples\033[0m\n"
-		echo "    $ sh $0"
-		echo "    $ sh $0 -o"
-		echo
-		exit 2
-		;;
-	esac
-done
-
-if [ -e $nvim_config_path -o -e $nvim_resource_path ]; then
-	echo "\033[1;33mFound existing neovim config\033[0m"
-	echo
-	echo "  $nvim_config_path"
-	echo "  $nvim_resource_path"
-	echo
-	exit 2
 fi
 
 if [ $(which nvim) ]; then
