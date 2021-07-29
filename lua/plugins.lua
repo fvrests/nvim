02f82ed038a4d6dee91500d891cc7aae96495c07
mvllow <mvllow@icloud.com>
2021-07-28 21:14:47 -0500

    replace vim-commentary with nvim-comment

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -144,7 +144,17 @@ packer.startup(function(use)
 		end,
 	})
 
-	use('tpope/vim-commentary')
+	-- use('tpope/vim-commentary')
+	use({
+		'terrortylor/nvim-comment',
+		config = function()
+			require('nvim_comment').setup({
+				hook = function()
+					require('ts_context_commentstring.internal').update_commentstring()
+				end,
+			})
+		end,
+	})
 
 	use({
 		'lewis6991/gitsigns.nvim',
