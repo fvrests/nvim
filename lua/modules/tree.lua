cb7ac7f8d971f5163810d11c333634ec701ab5a0
mvllow <mvllow@icloud.com>
2021-08-02 16:31:15 -0500

    fix icon path

--- lua/modules/tree.lua
+++ lua/modules/tree.lua
@@ -16,7 +16,7 @@ vim.g.nvim_tree_show_icons = {
 	folder_arrows = 0,
 }
 
-vim.g.nvim_tree_symlink_arrow = O.icons.symlink
+vim.g.nvim_tree_symlink_arrow = O.editor.icons.symlink
 
 vim.g.nvim_tree_icons = {
 	default = '',
