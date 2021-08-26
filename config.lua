626a88ac237bbf0f6729c2512c6156c89185c6c3
fvrests <fvrests@icloud.com>
2021-08-26 15:27:59 -0500

    update config

--- config.lua
+++ config.lua
@@ -1,25 +1,16 @@
 -- these are some commonly changed options
 -- defaults can be found in lua/globals.lua
 
-vim.g.rose_pine_disable_italics = true
 vim.g.rose_pine_variant = 'dawn'
 O.colorscheme = 'rose-pine'
 O.format_on_save = true
-O.lsp = {
-	diagnostics = {
-		signs = true,
-		underline = true,
-		update_in_insert = false,
-		virtual_text = false,
-	},
-	servers = {
-		'lua',
-		'typescript',
-		'html',
-		'svelte',
-		'css',
-		'tailwindcss',
-		'json',
-		'go',
-	},
+O.lsp.servers = {
+	'lua',
+	'typescript',
+	'html',
+	'svelte',
+	'css',
+	'tailwindcss',
+	'json',
+	'vue',
 }
